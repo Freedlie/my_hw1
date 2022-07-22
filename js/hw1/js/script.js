12e64e949abe3f9f3db38956e60060d13872d5f8
Freedlie <kolich.m2003@gmail.com>
2022-07-22 12:43:02 +0300

    js hw1

---
+++ js/hw1/js/script.js
@@ -0,0 +1,116 @@
+let str1 = 'hello';
+console.log(str1);
+alert(str1);
+document.write(str1);
+
+document.write(' ');
+
+let str2 = 'owu';
+console.log(str2);
+alert(str2);
+document.write(str2);
+
+document.write('.');
+
+
+let str3 = 'com';
+console.log(str3);
+alert(str3);
+document.write(str3);
+
+document.write('.');
+
+
+let str4 = 'ua';
+console.log(str4);
+alert(str4);
+document.write(str4);
+
+document.write(' ');
+
+let num1 = 1;
+console.log(num1);
+alert(num1);
+document.write(`${num1}`);
+
+document.write(' ');
+
+let num2 = 10;
+console.log(num2);
+alert(num2);
+document.write(`${num2}`);
+
+document.write(' ');
+
+let num3 = -999;
+console.log(num3);
+alert(num3);
+document.write(`${num3}`);
+
+document.write(' ');
+
+let num4 = 123;
+console.log(num4);
+alert(num4);
+document.write(`${num4}`);
+
+document.write(' ');
+
+let num5 = 3.14;
+console.log(num5);
+alert(num5);
+document.write(`${num5}`);
+
+document.write(' ');
+
+let num6 = 2.7;
+console.log(num6);
+alert(num6);
+document.write(`${num6}`);
+
+document.write(' ');
+
+let num7 = 16;
+console.log(num7);
+alert(num7);
+document.write(`${num7}`);
+
+document.write(' ');
+
+let bool1 = true;
+console.log(bool1);
+alert(bool1);
+document.write(`${bool1}`);
+
+document.write(' ');
+
+let bool2 = false;
+console.log(bool2);
+alert(bool2);
+document.write(`${bool2}`);
+
+// ---------------------------------------------------------------------
+
+let firstName = 'Maksym';
+let middleName = 'Romanovych';
+let lastName = 'Kolych';
+let person = `${lastName} ${firstName} ${middleName}`;
+console.log(person);
+
+// ---------------------------------------------------------------------
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// ---------------------------------------------------------------------
+
+let age = '18';
+
+prompt(firstName);
+prompt(middleName);
+prompt(age);
